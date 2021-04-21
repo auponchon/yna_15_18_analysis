a0a6e70ffad328430cfb4976dca8d629ed0533b3
auponchon <aurore.ponchon@gmail.com>
2021-04-21 17:49:18 +0200

    Nest table

--- R scripts/foraging and resting locations to shapefiles for maxent.R
+++ R scripts/foraging and resting locations to shapefiles for maxent.R
@@ -1,33 +1,42 @@
 load("Data/NewlyCreatedData/ForRestLocFail2015.RData")
+library(sf)
+library(rgdal)
 
-sf_locs.1 <- sf::st_as_sf(loc.2015.fail, coords = c("Longitude","Latitude")) %>% 
-    sf::st_set_crs(4326)
+loc.2015.fail$x<-loc.2015.fail$Longitude
+loc.2015.fail$y<-loc.2015.fail$Latitude
 
-st_write(sf_locs.1,layer="loc_for_fail_2015",dsn="Maxent modelling/Maxent 2015/Occurence/RarefyOccurence",
-         driver="ESRI Shapefile", delete_layer = TRUE,append=T)
+coordinates(loc.2015.fail)<-c("Longitude","Latitude")
+proj4string(loc.2015.fail) <- CRS("+proj=longlat +ellps=WGS84")
 
+writeOGR(loc.2015.fail, dsn="Maxent modelling/Maxent 2015/Occurence/RarefyOccurence",
+         layer="loc_for_fail_2015", driver="ESRI Shapefile",overwrite_layer=T)
 
 load("Data/NewlyCreatedData/ForRestLocSuccess2015.RData")
+loc.2015.suc$x<-loc.2015.suc$Longitude
+loc.2015.suc$y<-loc.2015.suc$Latitude
 
-sf_locs.2 <- sf::st_as_sf(loc.2015.suc, coords = c("Longitude","Latitude")) %>% 
-    sf::st_set_crs(4326)
+coordinates(loc.2015.suc)<-c("Longitude","Latitude")
+proj4string(loc.2015.suc) <- CRS("+proj=longlat +ellps=WGS84")
 
-st_write(sf_locs.2,layer="loc_for_suc_2015",dsn="Maxent modelling/Maxent 2015/Occurence/RarefyOccurence",
-         driver="ESRI Shapefile", delete_layer = TRUE,append=T)
+writeOGR(loc.2015.suc, dsn="Maxent modelling/Maxent 2015/Occurence/RarefyOccurence",
+         layer="loc_for_suc_2015", driver="ESRI Shapefile",overwrite_layer=T)
 
 load("Data/NewlyCreatedData/ForRestLocFail2018.RData")
+loc.2018.fail$x<-loc.2018.fail$Longitude
+loc.2018.fail$y<-loc.2018.fail$Latitude
 
-sf_locs.3 <- sf::st_as_sf(loc.2018.fail, coords = c("Longitude","Latitude")) %>% 
-    sf::st_set_crs(4326)
+coordinates(loc.2018.fail)<-c("Longitude","Latitude")
+proj4string(loc.2018.fail) <- CRS("+proj=longlat +ellps=WGS84")
 
-st_write(sf_locs.3,layer="loc_for_fail_2018",dsn="Maxent modelling/Maxent 2018/Occurence/RarefyOccurence",
-         driver="ESRI Shapefile", delete_layer = TRUE,append=T)
-
+writeOGR(loc.2018.fail, dsn="Maxent modelling/Maxent 2018/Occurence/RarefyOccurence",
+         layer="loc_for_fail_2018", driver="ESRI Shapefile",overwrite_layer=T)
 
 load("Data/NewlyCreatedData/ForRestLocSuccess2018.RData")
+loc.2018.suc$x<-loc.2018.suc$Longitude
+loc.2018.suc$y<-loc.2018.suc$Latitude
 
-sf_locs.4 <- sf::st_as_sf(loc.2018.suc, coords = c("Longitude","Latitude")) %>% 
-    sf::st_set_crs(4326)
+coordinates(loc.2018.suc)<-c("Longitude","Latitude")
+proj4string(loc.2018.suc) <- CRS("+proj=longlat +ellps=WGS84")
 
-st_write(sf_locs.4,layer="loc_for_suc_2018",dsn="Maxent modelling/Maxent 2018/Occurence/RarefyOccurence",
-         driver="ESRI Shapefile", delete_layer = TRUE,append=T)
+writeOGR(loc.2018.suc, dsn="Maxent modelling/Maxent 2018/Occurence/RarefyOccurence",
+         layer="loc_for_suc_2018", driver="ESRI Shapefile",overwrite_layer=T)
